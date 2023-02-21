ef64e0699076b3916c222016d3ae72879b60c64d
zyl <2392505041@qq.com>
2023-02-21 16:39:22 +0800

    更新时间测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,13 @@
 module.exports = {
     // base: "/myblogtest/",
     themeConfig: {
-      logo: '/assets/img/logo.png',
-      //  导航栏链接
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Guide', link: '/guide/' },
-        { text: 'External', link: 'https://google.com' },
-      ]
+        astUpdated: '最后更新时间',
+        logo: '/assets/img/logo.png',
+        //  导航栏链接
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'External', link: 'https://google.com'},
+        ]
     }
-  }
+}
