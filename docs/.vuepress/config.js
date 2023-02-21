af076eacbdd8ca58c464ce508cf175bc12efde58
6-Spring <77000969+6-spring@users.noreply.github.com>
2023-02-21 14:03:31 +0800

    测试01

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    base: "/myblogtest/",
+    themeConfig: {
+      logo: '/assets/img/logo.png',
+      //  导航栏链接
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }
+  }
---
+++ docs/about.md
@@ -0,0 +1,14 @@
+---
+sidebar: auto
+title: 关于我
+---
+
+## 第一章
+### 1、java基础
+## 第二章
+### 1、MySQL基础
+## 第三章
+### 1、spring
+### 2、Springboot
+### 3、Springcloud
+#### 3.1、nacos基础
