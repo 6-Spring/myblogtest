fd48c0bf205258c71c8ae59fd7c8b6fb4dc2bb17
zyl <2392505041@qq.com>
2023-02-21 16:53:46 +0800

    更新时间中文域名测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 module.exports = {
     // base: "/myblogtest/",
     themeConfig: {
-        astUpdated: '最后更新时间',
+        lastUpdated: '更新时间',
         logo: '/assets/img/logo.png',
         //  导航栏链接
         nav: [
